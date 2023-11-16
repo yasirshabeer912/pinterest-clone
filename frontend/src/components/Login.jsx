cbce1f2bf272013484ba6d7092147aec9fe23a01
Muhammad Yasir <imyasirshabeer@gmail.com>
2023-11-17 00:27:43 +0500

    auth working completed state remaning

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import { useState } from "react";
 import { FaPinterest } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-const Login = () => {
+import Modal from "react-bootstrap/Modal";
+const Login = ({ show, setShow }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -13,6 +15,7 @@ const Login = () => {
       const response = await axios.post(apiUrl, { email, password });
       if (response && response.data) {
         navigate("/posts");
+        setShow(false);
       } else {
         console.error("Invalid response format");
       }
@@ -22,63 +25,46 @@ const Login = () => {
   };
   return (
     <div>
-      <div
-        className="modal fade py-5"
-        id="exampleModal"
-        tabIndex={"-1"}
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog  modal-dialog-centered">
-          <div className="modal-content ">
-            <div className="modal-header">
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <form action="" onSubmit={handleSubmit}>
-              <div className="modal-body px-5 border-0 pb-5">
-                <div className="modelAuth px-5">
-                  <FaPinterest className="" />
-                  <div className="h1 text-nowrap my-4">
-                    Welcome To Pinterest
-                  </div>
-                  <div className="mb-3 w-100">
-                    <label className="form-label">Email</label>
-                    <input
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="email"
-                      className="form-control w-100"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="mb-3 w-100">
-                    <label className="form-label">Password</label>
-                    <input
-                      type="password"
-                      onChange={(e) => setPassword(e.target.value)}
-                      className="form-control w-100"
-                      id="exampleInputEmail1"
-                      aria-describedby="emailHelp"
-                      placeholder="Password"
-                    />
-                  </div>
-                  <button type="submit" className="btn authBtn">
-                    LOGIN
-                  </button>
-                  <div className="or text-center my-2">OR</div>
-                  <div className="btn authBtn google">Continue With Google</div>
-                </div>
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form action="" onSubmit={handleSubmit}>
+            <div className="modelAuth px-5">
+              <FaPinterest className="" />
+              <div className="h1 text-nowrap my-4">Welcome To Pinterest</div>
+              <div className="mb-3 w-100">
+                <label className="form-label">Email</label>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  className="form-control w-100"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Email"
+                />
               </div>
-            </form>
-          </div>
-        </div>
-      </div>
+              <div className="mb-3 w-100">
+                <label className="form-label">Password</label>
+                <input
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="form-control w-100"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" className="btn authBtn">
+                LOGIN
+              </button>
+              <div className="or text-center my-2">OR</div>
+              <div className="btn authBtn google">Continue With Google</div>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,99 +1,90 @@
 import axios from "axios";
 import { useState } from "react";
 import { FaPinterest } from "react-icons/fa";
+import Modal from "react-bootstrap/Modal";
+const Signup = ({showW,setShowW}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-const Signup = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const apiUrl = "http://localhost:8000/api/users/";
+      const { data } = await axios.post(apiUrl, {
+        email,
+        password,
+        confirmPassword,
+      });
+      setShowW(false)
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const apiUrl = 'http://localhost:8000/api/users/';
-            const { data } = await axios.post(apiUrl, { email, password, confirmPassword });
-            console.log(data);
-        } catch (error) {
-            console.error(error.response.data.message);
-        }
-    };
-
-    return (
-        <>
-            <div
-                className="modal fade py-5"
-                id="SignUpModel"
-                tabIndex={"-1"}
-                aria-labelledby="exampleModalLabel"
-                aria-hidden="true"
-            >
-                <div className="modal-dialog  modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button
-                                type="button"
-                                className="btn-close"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            ></button>
-                        </div>
-                        <form onSubmit={handleSubmit}>
-                            <div className="modal-body px-5 border-0 pb-5">
-                                <div className="modelAuth px-5">
-                                    <FaPinterest className="" />
-                                    <div className="h my-4">
-                                        <div className="h1 text-nowrap">Welcome To Pinterest</div>
-                                        <div className="p text-secondary text-center">
-                                            Find New Ideas To Try
-                                        </div>
-                                    </div>
-                                    <div className="mb-3 w-100">
-                                        <label className="form-label">Email</label>
-                                        <input
-                                            type="email"
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            required
-                                            className="form-control w-100"
-                                            id="exampleInputEmail1"
-                                            aria-describedby="emailHelp"
-                                            placeholder="Email"
-                                        />
-                                    </div>
-                                    <div className="mb-3 w-100">
-                                        <label className="form-label">Password</label>
-                                        <input
-                                            type="password"
-                                            required
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            className="form-control w-100"
-                                            id="exampleInputEmail1"
-                                            aria-describedby="emailHelp"
-                                            placeholder="Password"
-                                        />
-                                    </div>
-                                    <div className="mb-3 w-100">
-                                        <label className="form-label">Confirm Password</label>
-                                        <input
-                                            type="password"
-                                            required
-                                            onChange={(e) => setConfirmPassword(e.target.value)}
-                                            className="form-control w-100"
-                                            id="exampleInputEmail1"
-                                            aria-describedby="emailHelp"
-                                            placeholder="Confirm Password"
-                                        />
-                                    </div>
-                                    <button type="submit" className="btn authBtn">LOGIN</button>
-                                    <div className="or text-center my-2">OR</div>
-                                    <div className="btn authBtn google">Continue With Google</div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+  return (
+    <>
+      <Modal show={showW} onHide={() => setShowW(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+              <div className="modelAuth px-5">
+                <FaPinterest className="" />
+                <div className="h my-4">
+                  <div className="h1 text-nowrap">Welcome To Pinterest</div>
+                  <div className="p text-secondary text-center">
+                    Find New Ideas To Try
+                  </div>
                 </div>
-            </div>
-        </>
-    );
+                <div className="mb-3 w-100">
+                  <label className="form-label">Email</label>
+                  <input
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="form-control w-100"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="mb-3 w-100">
+                  <label className="form-label">Password</label>
+                  <input
+                    type="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control w-100"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="mb-3 w-100">
+                  <label className="form-label">Confirm Password</label>
+                  <input
+                    type="password"
+                    required
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    className="form-control w-100"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+                <button type="submit" className="btn authBtn">
+                  LOGIN
+                </button>
+                <div className="or text-center my-2">OR</div>
+                <div className="btn authBtn google">Continue With Google</div>
+              </div>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 };
 
 export default Signup;
