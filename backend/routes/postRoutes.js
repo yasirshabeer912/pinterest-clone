32f3a774f1a563feb9df6646e07e997cc6c85a43
codeblue12 <yasir.codeblue@gmail.com>
2024-01-13 15:37:11 +0500

    api deployment issue- multer path change

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -6,7 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const verifyToken = require('../middleware/verifyToken');
 // Set up multer to store files in the 'uploads' folder within the 'backend' folder
-const uploadFolder = path.join(__dirname, '../uploads/');
+const uploadFolder = path.join(__dirname, '../tmp');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Create the 'uploads' folder if it doesn't exist
